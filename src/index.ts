99562cfea80d25910565143685dacba9cd84998e
Hyeminah <144677544+Hyeminah@users.noreply.github.com>
2023-11-23 14:12:36 +0100

    Nolwenn/user registration (#38)
    Co-authored-by: Noémie Mateu <noemiemateu@yahoo.fr>

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,15 @@ const port = 8000
 app.set('view engine', 'pug');
 app.set('views', './src/views')
 app.get('/inscription', (req: Request, res: Response) => {
-  res.render('index', {});
+  res.render('index', {pageTitle: 'Inscription'});
+});
+app.get('/home', (req: Request, res: Response) => {
+  res.render('home', {pageTitle: 'Home'});
+});
+app.get('/login', (req: Request, res: Response) => {
+  res.render('login', {pageTitle: 'Connexion'});
 });
 
 app.listen(port, () => {
-  console.log(`Server is Fire at http://localhost:${port}`);
+  console.log(`Server is Fire at http://localhost:${port}/home`);
 });
