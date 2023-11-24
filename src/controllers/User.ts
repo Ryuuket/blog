1cd6eb5eb8d1a15862838631e36f86e49600b7a0
Axel Galvier <axelgalvier0@gmail.com>
2023-11-24 12:00:31 +0100

    Refactoration in class of User.

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -1,13 +1,14 @@
 import * as crypto from "crypto";
 
-function hashPassword(password: string): string {
-
+class Users {
     // Creating a random salt
-    const salt: string = "ya64hs"; 
+    static salt: string = "ya64hs"; 
     
-    // Hash the salt and password with 10000 iterations, 64 length and sha512 digest 
-    const hash: string = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
-
-    return hash;
+    // Hash the salt and password with 10000 iterations, 64 length and sha512 digest
+    static hashPassword(password: string): string {
+        const hash: string = crypto.pbkdf2Sync(password, Users.salt, 10000, 64, 'sha512').toString('hex');
+    
+        return hash;
+    }
 
 }
