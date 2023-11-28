66b1ab23931994f89f6d8f2f251d4c22de84c9cb
eric corbisier <ecorbisier.simplon@gmail.com>
2023-11-28 12:07:35 +0100

    Initialise les variables de récupération des POST pour le login et register

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,14 +1,15 @@
-import express, { Express, Request, Response, Application } from 'express';
-import { app } from '../index';
+import express, { Express, Request, Response, Application } from "express";
+import { app } from "../index";
 
-app.get('/login', (req: Request, res: Response) => {
-    res.render('login', { pageTitle: 'Connexion' });
+app.get("/login", (req: Request, res: Response) => {
+  res.render("login", { pageTitle: "Connexion" });
 });
 
-app.get('/submit-login', async (req: Request, res: Response) => {
-    // Récupération des données du formulaire à partir de la requête
-    const login = req.query.login as string;
-    const password = req.query.password as string;
+export let login: string;
+export let password: string;
 
-
+app.get("/submit-login", async (req: Request, res: Response) => {
+  // Récupération des données du formulaire à partir de la requête
+  login = req.query.login as string;
+  password = req.query.password as string;
 });
--- src/routes/register.ts
+++ src/routes/register.ts
@@ -1,18 +1,28 @@
-import express, { Express, Request, Response, Application } from 'express';
-import { app } from '../index';
+import express, { Express, Request, Response, Application } from "express";
+import { app } from "../index";
 
-app.get('/inscription', (req: Request, res: Response) => {
-    res.render('register', { pageTitle: 'Inscription' });
+app.get("/inscription", (req: Request, res: Response) => {
+  res.render("register", { pageTitle: "Inscription" });
 });
 
-app.get('/submit-register', async (req: Request, res: Response) => {
-    // Récupération des données du formulaire à partir de la requête
-    const password = req.query.password as string;
-    const email = req.query.email as string;
-    const pseudo = req.query.pseudo as string;
-    const lastname = req.query.lastname as string;
-    const firstname = req.query.firstname as string;
-    const confirmation = req.query.confirmation as string;
+export let password: string;
+export let email: string;
+export let pseudo: string;
+export let lastname: string;
+export let firstname: string;
+export let confirmation: string;
+export let keypassword: string;
+export let isAdmin: boolean;
 
-
+app.get("/submit-register", async (req: Request, res: Response) => {
+  // Récupération des données du formulaire à partir de la requête
+  lastname = req.query.lastname as string;
+  firstname = req.query.firstname as string;
+  pseudo = req.query.pseudo as string;
+  email = req.query.email as string;
+  password = req.query.password as string;
+  confirmation = req.query.confirmation as string;
+  isAdmin = false;
+  const currentDate: Date = new Date();
+  const isValid: boolean = false;
 });
