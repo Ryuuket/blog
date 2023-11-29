77e506b0609f30426b2bd9f2d99e5e48b6d2ba48
Axel Galvier <axelgalvier0@gmail.com>
2023-11-29 10:07:18 +0100

    Added password hashing.

--- src/controllers/UserHash.ts
+++ src/controllers/UserHash.ts
@@ -1,12 +1,12 @@
 import * as crypto from "crypto";
 
-export class UserController {
+export class UserHash {
     // Creating a random salt
     static salt: string = "ya64hs"; 
     
     // Hash the salt and password with 10000 iterations, 64 length and sha512 digest
     static hashPassword(password: string): string {
-        const hash: string = crypto.pbkdf2Sync(password, UserController.salt, 10000, 64, 'sha512').toString('hex');
+        const hash: string = crypto.pbkdf2Sync(password, UserHash.salt, 10000, 64, 'sha512').toString('hex');
     
         return hash;
     }
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,16 +1,15 @@
-import db from '../database';
-import { UserController } from '../controllers/UserController';
+import client from '../database';
+import { UserHash } from '../controllers/UserHash';
+import {firstname, lastname, pseudo, email, password, is_admin} from "../routes/register";
 
 class User{
-    static idCount: number;
     
-    async register (firstname: string, lastname: string, email: string, pseudo: string, password: string): Promise<boolean> {
+    async register (): Promise<boolean> {
         try {
-            const values = [firstname, lastname, email, pseudo, UserController.hashPassword(password)];
+            const values = [firstname, lastname, email, pseudo, UserHash.hashPassword(password), is_admin];
             const query = 'INSERT INTO formulaire (last_name, first_name, email, pseudo, key_password, is_admin,) VALUES ($1, $2,$3, $4,$5,$6)';
         
-            await db.query(query, values);
-            
+            await client.query(query, values);
           } catch (error) {
             console.error('Erreur lors de la sauvegarde des données :', error);
             return false
