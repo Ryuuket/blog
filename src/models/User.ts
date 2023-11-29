19dc26a4e8933181377a31632b453c09d55f56ee
Ryuuket <144806364+Ryuuket@users.noreply.github.com>
2023-11-29 11:59:01 +0100

    Data register (#60)

---
+++ src/models/User.ts
@@ -0,0 +1,19 @@
+import client from '../database';
+import { UserHash } from '../controllers/UserHash';
+import {firstname, lastname, pseudo, email, password, is_admin} from "../routes/register";
+
+class User{
+    
+    async register (): Promise<boolean> {
+        try {
+            const values = [firstname, lastname, email, pseudo, UserHash.hashPassword(password), is_admin];
+            const query = 'INSERT INTO formulaire (last_name, first_name, email, pseudo, key_password, is_admin,) VALUES ($1, $2,$3, $4,$5,$6)';
+        
+            await client.query(query, values);
+          } catch (error) {
+            console.error('Erreur lors de la sauvegarde des données :', error);
+            return false
+          }
+        return true;
+    };
+}
