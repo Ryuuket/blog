af6667172c83d202ce180906e42f2f91e8359e94
ecorbisierSimplon <144805223+ecorbisierSimplon@users.noreply.github.com>
2023-11-30 16:21:37 +0100

    Resolve conflit index.ts (#89)

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ export const app: Application = express();
 export const port = 8000;
 
 
-<<<<<<< Updated upstream
 app.use(session({
   secret: 'your-secret-key',
   resave: false,
@@ -38,16 +37,4 @@ import "./routes/home";
 import "./routes/login";
 import "./routes/logout";
 import "./routes/register";
-=======
-// Configuration of the Pug rendering engine
-app.set('view engine', 'pug');
-app.set('views', './src/views');
-app.listen(port, () => {
-  console.log('Server is Fire at http://localhost:' + port);
-});
-
-import './routes/home';
-import './routes/login';
-import './routes/register';
 import './routes/blog';
->>>>>>> Stashed changes
