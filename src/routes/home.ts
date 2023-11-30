45ce046a5741b952a92ba277ce6fc3c51ff030bf
ecorbisierSimplon <144805223+ecorbisierSimplon@users.noreply.github.com>
2023-11-30 10:32:53 +0100

    Session variable/#62 (#78)

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -1,6 +1,11 @@
 import express, { Express, Request, Response, Application } from 'express';
 import { app } from '../index';
 
+
+
 app.get('/', (req: Request, res: Response) => {
-    res.render('home', { pageTitle: 'Home' });
+
+    const isUserLoggedIn = req.session.loginIsValid || false;
+
+    res.render('home', { pageTitle: 'Home', isUserLoggedIn: isUserLoggedIn });
 });
--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,5 +1,8 @@
 import express, { Express, Request, Response, Application } from "express";
+import session from 'express-session';
 import { app } from "../index";
+
+
 
 app.get("/login", (req: Request, res: Response) => {
   res.render("login", { pageTitle: "Connexion" });
@@ -12,4 +15,23 @@ app.get("/submit-login", async (req: Request, res: Response) => {
   // Récupération des données du formulaire à partir de la requête
   login = req.query.login as string;
   password = req.query.password as string;
+
+  const loginIsValid: boolean = false;
+  const loginIsAdmin: boolean = false;
+
+  // Classe controle Variable
+
+  // Si controle ok :
+  // Classe controle login et password
+
+  if (loginIsValid) {
+    req.session.loginIsValid = true;
+    if (loginIsAdmin) {
+      req.session.loginIsAdmin = true;
+    }
+  }
+  const isUserLoggedIn = req.session.loginIsValid || false;
+
+  res.render('home', { pageTitle: 'Home', isUserLoggedIn: isUserLoggedIn });
+
 });
