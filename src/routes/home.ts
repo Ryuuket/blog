525dc5b28098c3c6d8aabf80242c99780d9e5b13
Noémie Mateu <noemiemateu@yahoo.fr>
2023-12-01 09:17:16 +0100

    css blog, ajout du lien du bouton, changement des routes isAdmin

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -6,6 +6,8 @@ import { app } from '../index';
 app.get('/', (req: Request, res: Response) => {
 
     const isUserLoggedIn = req.session.loginIsValid || false;
+    const isUserAdmin = req.session.loginIsAdmin || false;
 
-    res.render('home', { pageTitle: 'Home', isUserLoggedIn: isUserLoggedIn });
+
+    res.render('home', { pageTitle: 'Home', isUserLoggedIn: isUserLoggedIn, isUserAdmin: isUserAdmin });
 });
